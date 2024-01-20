db84025dd67600ff1b97fcb337b37c6832e5fa0a
wambexpeter <wambexpeter@gmail.com>
2024-01-20 16:38:54 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./Components/LoginForm";
+import ProfilePage from "./Components/ProfilePage";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [userId, setUserId] = useState(null);
+
+  const handleLogin = (id) => {
+    setUserId(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!userId ? (
+        <LoginForm onLogin={handleLogin} />
+      ) : (
+        <ProfilePage userId={userId} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
